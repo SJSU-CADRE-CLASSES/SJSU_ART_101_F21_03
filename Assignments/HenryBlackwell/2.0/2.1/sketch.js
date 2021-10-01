4119b45bdc84924aab1bc168b330eb33b83e269d
Henry Blackwell <alecgammon@gmail.com>
2021-09-30 20:09:11 -0700

    update

--- Assignments/HenryBlackwell/2.0/2.1/sketch.js
+++ Assignments/HenryBlackwell/2.0/2.1/sketch.js
@@ -4,9 +4,20 @@ function setup() {
   createCanvas(400, 400);
   background (200);
 
-  console log (games[2])
+
+  games[1] = "Apex Legends";
+
+  console.log ("initial array is");
+  console.log(games);
+
+
+games.splice(2,1); //games.splice(index(Starting number),number(how many to take out))
+console.log("Array after splice")
+console.log(games);  
 }
 
 function draw() {
   background(220);
+
+
 }
