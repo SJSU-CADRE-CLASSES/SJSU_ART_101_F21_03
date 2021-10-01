a1ec6460e6d56e76439d480624ff9d636411ffd7
Henry Blackwell <alecgammon@gmail.com>
2021-09-30 21:20:18 -0700

    update

--- Assignments/HenryBlackwell/2.0/2.1/2b.5-2b.8/sketch.js
+++ Assignments/HenryBlackwell/2.0/2.1/2b.5-2b.8/sketch.js
@@ -24,16 +24,28 @@ let randomIndex;
 
 function setup() {
   createCanvas(400, 400);
-    background(220);
+  background(220);
+  textSize(32);
+
+  text("Click to Randomize",)
 
 }
 
 function draw() {
+
 }
 
 function mousePressed(){
+
+ if (games[0]){ 
+  //This shows a random name and splies it our of array
   background(random(200, 255));
   randomIndex = int(random(games.length));
   text(games[randomIndex].name, 50, 50)
-  // games.splice(randomIndex, 1)
+  games.splice(randomIndex, 1)
+ } else{
+  background(random(200, 255));
+  text("nothing left!", 50, 50)
+ }
+
 }
