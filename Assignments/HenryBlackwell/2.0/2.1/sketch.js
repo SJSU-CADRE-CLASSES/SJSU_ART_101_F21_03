42baee71ab981c17ca0f952e3fadc99b5c9905f9
Henry Blackwell <alecgammon@gmail.com>
2021-09-30 19:18:26 -0700

    Uploading new sketch

---
+++ Assignments/HenryBlackwell/2.0/2.1/sketch.js
@@ -0,0 +1,12 @@
+let games = ["Destiny", "Apex", "Hearthstone", "Warhammer", "Splitgate", "Phasmo"]
+
+function setup() {
+  createCanvas(400, 400);
+  background (200);
+
+  console log (games[2])
+}
+
+function draw() {
+  background(220);
+}
