fc99575f25bdd109c4b7cf9cb49006d73b22ad89
JoodanRe <jordan.le@sjsu.edu>
2021-10-04 15:50:39 -1000

    Starting 2.1b
    just creating arrays

--- Assignments/JordanLe/2.1/sketch.js
+++ Assignments/JordanLe/2.1/sketch.js
@@ -1,10 +1,15 @@
+let technicalSoftware = ["Computer Programming", "OOP", "data structures", "algorithms", "OOD", "Assembly Language/Machine Code", "web development", "mobile app development", "game development", "Excel Sheets", "Database Management Systems", "Software Engineering", "Management Information Systems", "Informational Technology"];
+//let bothSoftware = [""];
+let artisticSoftware = ["Creative Coding", "3D Modeling", "web aesthetics", "game aesthetics", "digital animation", "Photo editing", "video editing", "digital painting", "Computer Graphics", "Digital Media Art", "Graphic Design", "music producer"];
+let ghostsOfLife = ["Blue Ghost", "Boo", "You've been pranked", "Muni Munyi", "Ghost of Time", "Seoul Ghost", "loneliness", "basketcase", "determination", "passionate", "niceness", "quirky", "life lessons", "philosohpies", "curious"];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(120);
-  noStroke();
-  fill(150,50,59);
-  ellipse(100,100,40);
+  background(60, 140, 250);
+  //noStroke();
+  fill(250, 140, 60);
+  ellipse(100, 100, 40);
 }
