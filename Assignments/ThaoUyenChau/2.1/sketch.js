fc9962af8918aeffb3362bc18d586eff89a20dc9
ThaoU-C <89234526+ThaoU-C@users.noreply.github.com>
2021-10-05 20:39:39 -0700

    Update sketch.js

--- Assignments/ThaoUyenChau/2.1/sketch.js
+++ Assignments/ThaoUyenChau/2.1/sketch.js
@@ -21,17 +21,45 @@ let steps = [{
   step: "325",
   day: "Tues"
 }];
+
+let randomIndex;
+let animating = false;
+
 function setup() {
 
   createCanvas(600, 600);
   background (200);
+  textSize(32);
 
-console.log(steps[1].day);
+  text("click to randomize", 50,50);
 
+// console.log(random(5));
 
 }
 
 
 function draw() {
+
+ if (animating == true){
+   ellipse(random(width), random(height), random(50, 200));
+ }
+}
+function randomizer(){
+  animating = false;
+
+  if (steps[0]){
+    background(random(200, 255));
+    randomIndex = int(random(steps.length));
+    // console.log(steps[randomIndex].day);
+    text(`${steps[randomIndex].day} I walked ${steps[randomIndex].step} steps`, 50, 50);
+    steps.splice(randomIndex, 1);
+  } else {
+    background(random(200, 255));
+    text("nothing left", 50, 50);
+  }
+}
+function mousePressed() {
+animating = true
+setTimeout(randomizer, 2000);
 
 }
