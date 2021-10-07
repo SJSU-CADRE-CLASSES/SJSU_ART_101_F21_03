28c4104a3d5c3458b025106b30b1904d3ba1a1c8
Henry Blackwell <alecgammon@gmail.com>
2021-10-06 18:47:11 -0700

    .

--- Assignments/HenryBlackwell/2.0/P5Template/sketch.js
+++ Assignments/HenryBlackwell/2.0/P5Template/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(120);
-  noStroke();
-  fill(150,50,59);
-  ellipse(100,100,40);
+
+
+  triangle(width/2, height/1.5, width/1.6, height/2.65, width/2.5, height/2.65)
 }
---
+++ Assignments/HenryBlackwell/2.0/Working Randomizer/sketch.js
@@ -0,0 +1,84 @@
+//command+b will make code nice
+
+let games = [{
+  name: "Destiny 2",
+  thing: "Traveler"
+}, {
+  name: "Apex Legends",
+  thing: "Legend"
+}, {
+  name: "Hearthstone",
+  thing: "Card"
+}, {
+  name: "Warhammer 40k",
+  thing: "Forest"
+}, {
+  name: "Splitgate",
+  thing: "Portal"
+}, {
+  name: "Phasmophoria",
+  thing: "Wall"
+}, {
+  name: "New World",
+  thing: "Sword"
+}, {
+  name: "Deathloop",
+  thing: ""
+}, {
+  name: "Hades",
+  thing: ""
+}, {
+  name: "Monster Hunter: World",
+  thing: ""
+}, {
+  name: "League of Legends",
+  thing: ""
+}
+]
+
+let randomIndex;
+let animating = false;
+
+function setup() {
+  createCanvas(700, 700);
+  background(128,128,128);
+  textSize(32);
+  angleMode(DEGREES)
+
+  fill (200,189,230)
+  textAlign(LEFT);
+  text(`Click to find what game 
+  you should play today`, width/5, height/5)
+
+}
+
+function draw() {
+
+if (animating == true){
+  fill(20,155,75)
+  ellipse (random(width), random(height), random(50,100))
+  }
+}
+
+function randomizer(){
+  animating = false;
+ if (games[0]){ 
+  //This shows a random name and splies it our of array
+  background(random(200, 255));
+  randomIndex = int(random(games.length));
+  fill(122,128,144)
+  textAlign(LEFT);
+  text(`Today, you will play ${games[randomIndex].name}`, width/5, height/6)
+  games.splice(randomIndex, 1)
+
+ } else{
+  background(random(200, 255));
+  textAlign(CENTER);
+  text("Nothing left!")
+ }
+}
+
+function mousePressed(){
+  animating = true;
+  setTimeout(randomizer, 3000);
+}
