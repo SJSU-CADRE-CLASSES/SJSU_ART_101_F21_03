f68787985992a5039ed0919b3c7ac281ac570a52
daisywu1129 <91577492+daisywu1129@users.noreply.github.com>
2021-10-09 11:05:08 -0700

    Update sketch.js

--- Assignments/JuichenWu/2.1b/sketch.js
+++ Assignments/JuichenWu/2.1b/sketch.js
@@ -21,12 +21,12 @@
 
     let randomIndex;
     let animating = false;
-    let trolls = []
+    let trolls =[]
     let imageCounter = 0;
 
-    function preload() {
+    function preload(){
 
-      for (let i = 0; i <= 15; i++) {
+      for (let i =0; i <=15; i++){
         trolls[i] = loadImage(`assets/troll_${i}.jpg`)
 
       }
@@ -50,36 +50,32 @@
 
       if (animating == true) {
         clear();
-        image(trolls[imageCounter], width / 2, height / 2);
+        image(trolls[imageCounter], width/2, height/2);
+        imageCounter++;
 
-        if (imageCounter < trolls.length) {
-          imageCounter++;
-          console. log(imageCounter);
-        } else {
-          imageCounter = 0;
-        }
       }
+    }
 
 
-      function randomizer() {
-        animating = false;
-        if (dogs[0]) {
-          //this displays random name and splices it out of array
-          background(random(200, 255));
-          randomIndex = int(random(dogs.length));
-          text(`${dogs[randomIndex].name} 's favorite color
+    function randomizer() {
+      animating = false;
+      if (dogs[0]) {
+        //this displays random name and splices it out of array
+        background(random(200, 255));
+        randomIndex = int(random(dogs.length));
+        text(`${dogs[randomIndex].name} 's favorite color
   ${dogs[randomIndex].color}`, 50, 50);
-          // text(dogs[randomIndex].name +"'s faviorits color is " +
-          // dogs[randomIndex].color, 50, 50);
-          dogs.splice(randomIndex, 1);
-        } else {
-          background(random(200, 255));
-          text("nothing left!", 50, 50)
-        }
+        // text(dogs[randomIndex].name +"'s faviorits color is " +
+        // dogs[randomIndex].color, 50, 50);
+        dogs.splice(randomIndex, 1);
+      } else {
+        background(random(200, 255));
+        text("nothing left!", 50, 50)
       }
+    }
 
-      function mousePressed() {
-        animating = true;
-        setTimeout(randomizer, 2000);
+    function mousePressed() {
+      animating = true;
+      setTimeout(randomizer, 2000);
 
-      }
+    }
