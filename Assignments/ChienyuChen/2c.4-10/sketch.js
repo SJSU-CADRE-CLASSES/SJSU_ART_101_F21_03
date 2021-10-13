19da70372028bb630f6b0c341de644c7c25b0543
vivianchen1212 <chentina1212@yahoo.com.tw>
2021-10-12 19:28:49 -0700

    2c.4-10

--- Assignments/ChienyuChen/2c.4-10/sketch.js
+++ Assignments/ChienyuChen/2c.4-10/sketch.js
@@ -2,38 +2,88 @@ let cats = [{
   name: "Chartreux",
   color: "blue"
 }, {
-  name: "Bombay", 
+  name: "Bombay",
   color: "Black"
 }, {
   name: "Siamese",
   color: "cream and chocolate"
 }
-}];
+];
 
 
 let randomIndex;
-//let animating = false;
-
-function setup() {
-createcanvas(600,600);
-background(200);
-textsize(40);
-text("click to randomize", 50, 50);
-}
+let animating = false;
+let trolls = [];
+let imageCounter = 0
+let button;
+let cnv;
+let nameInput;
 
 
-function draw() {
-  //if(animating == true){
-    //ellipse(random(width), random(height), random(50))
+
+
+function preload() {
+  for (let i = 1; i<=3 ; i++){
+    trolls[i] = loadImage(`2c.4-10/assets/troll_${i}.JPG`)
   }
 }
 
 
+
+
+function setup() {
+  //createCanvas(windowWidth, windowHeight);
+cnv = createCanvas(600,600);
+cnv.parent("#canvasDiv")
+
+background(20,40,200);
+textSize(32);
+//textFont('Courier new');
+textAlign(CENTER);
+textStyle(BOLD);
+fill(255);
+imageMode(CENTER);
+frameRate(8);
+
+text("click to randomize", width/2, height/2);
+
+button = select('#randButton');
+button.mousePressed(buttonPressed);
+button.class("randomizerButton");
+
+//nameInputs.push(createInput());
+nameInput = createInput();
+nameInput.parent("#inputFields");
+}
+
+
+
+
+function draw() {
+  if( animating == true){
+    clear();
+    image(trolls[imageCounter], width/2, height/2);
+
+    if (imageCounter < trolls.length - 1) {
+      imageCounter++;
+      console.log(imageCounter);
+    }
+    else {
+      imageCounter = 0;
+    }
+  }
+}
+
+
+
+
 function randomizer(){
+  animating = false
   if (cats[0]){
-    background(random(200, 255));
+  //  background(random(200, 255));
+  clear();
     randomIndex = int(random(cats.length));
-    text(cats[randomIndex].name, 50,50);
+    text(cats[randomIndex].name, width/2,height - 100);
     cats.splice(randomIndex, 1);
   }
   else {
@@ -43,31 +93,22 @@ function randomizer(){
 }
 
 
-function mousePressed() {
-//  animating = true;
+
+
+function buttonPressed() {
+  animating = true;
   setTimeout(randomizer, 2000);
 }
 
 
 
 
+//function changeBackground(){
+//  if (counter <= 2) {
+//    counter++;
+//    console.log(counter)
+//    background(random(255), random(255), random(255));
+//  }
+//  }
 
-function changeBackground(){
-  if (counter <= 2) {
-    counter++;
-    console.log(counter)
-    background(random(255), random(255), random(255));
-    setTimeout(changeBackground, 1000);
-  }
-  else {
-  }
-
-
-console.log(cats[2].color);
-
-function draw() {
-  background(120);
-  noStroke();
-  fill(150,50,59);
-  ellipse(100,100,40);
-}
+//}s
