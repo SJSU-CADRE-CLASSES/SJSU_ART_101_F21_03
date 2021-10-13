c6d88ddd89e7209c8783f206b5274504af19eb24
Alecblackwell1 <44239082+Alecblackwell1@users.noreply.github.com>
2021-10-12 22:44:55 -0700

    update

--- Assignments/HenryBlackwell/2.0/WorkingRandomizer/sketch.js
+++ Assignments/HenryBlackwell/2.0/WorkingRandomizer/sketch.js
@@ -1,6 +1,41 @@
 //command+b will make code nice
 
-let games = []
+let games = [{
+  name: "Apex Legends",
+  thing: "Legend"
+}, {
+  name: "Deathloop",
+  thing: ""
+}, {
+  name: "Destiny 2",
+  thing: "Traveler"
+}, {
+  name: "Hearthstone",
+  thing: "Card"
+}, {
+  name: "Warhammer 40k",
+  thing: "Forest"
+}, {
+  name: "Splitgate",
+  thing: "Portal"
+}, {
+  name: "Phasmophobia",
+  thing: "Wall"
+}, {
+  name: "New World",
+  thing: "Sword"
+}, {
+  name: "Hades",
+  thing: ""
+}, {
+  name: "Monster Hunter",
+  thing: ""
+}, {
+  name: "League of Legends",
+  thing: ""
+}]
+
+let inputGames = []
 
 let randomIndex;
 let animating = false;
@@ -8,10 +43,11 @@ let vgs = [];
 let imageCounter = 0;
 let tries = 0;
 let startRandomizerButton;
+let addMoreButton;
 let cnv;
 let nameInputs = [];
-let addMoreButton;
 
+//Loads the images beforehand
 function preload() {
   for (let i = 0; i <= 10; i++)
     vgs[i] = loadImage(`EditedAssets/vgs_${i}.jpg`)
@@ -29,8 +65,8 @@ function setup() {
   //Text at start
   fill(200, 189, 230)
   textAlign(LEFT);
-  text(`Click once to find what game 
-you should play today`, width / 5, height / 5)
+  text(`Input games you want to play
+  or click to randomize from a set list`, width / 5, height / 5)
 
   //Button to Randomize
   startRandomizerButton = select("#randButton");
@@ -41,7 +77,7 @@ you should play today`, width / 5, height / 5)
   addMoreButton.mousePressed(addAnotherInput);
 
   //Starts with 3 inputs
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 1; i++) {
     nameInputs.push(createInput())
     nameInputs[nameInputs.length - 1].parent("#inputFields");
   }
@@ -58,9 +94,7 @@ function draw() {
     } else {
       imageCounter = 0;
     }
-
   }
-
 }
 
 function addAnotherInput() {
@@ -71,33 +105,45 @@ function addAnotherInput() {
 
 function randomizer() {
   animating = false;
-  if (tries < 15) {
-    //This shows a random game
+  if (nameInputs.length == 1) {
+    if (tries < 15) {
+      //This shows a random game
+      clear();
+      randomIndex = int(random(games.length));
+      fill(122, 128, 144)
+      textAlign(CENTER);
+      textSize(32);
+      text(`Today, you will play`, width / 2, height / 20);
+      text('Refresh to add your own inputs', width / 2, height * 0.9)
+      //Display what game to play
+      text(games[randomIndex].name, width / 2, height / 10);
+      //Shows image for what game to play
+      image(vgs[randomIndex], width / 2, height / 2);
+      // text('Click once to reroll', width / 2, height / 1.17)
+      tries = tries + 1;
+
+    } else {
+      background(random(200, 255));
+      textAlign(CENTER);
+      fill(255, 0, 0)
+      text("You've tried too many times, pick one already!", width / 2, height / 5)
+    }
+  } else {
+    //This makes it cycle through the inputs    
     clear();
-    randomIndex = int(random(games.length));
-    fill(122, 128, 144)
     textAlign(CENTER);
     textSize(32);
+    randomIndex = int(random(inputGames.length))
     text(`Today, you will play`, width / 2, height / 20);
-    //Display what game to play
-    // text(games[randomIndex].name, width / 2, height / 10);
-    //Shows image for what game to play
-    image(vgs[randomIndex], width / 2, height / 2);
-    text('Click once to reroll', width / 2, height / 1.17)
-    tries = tries + 1;
-
-  } else {
-    background(random(200, 255));
-    textAlign(CENTER);
-    fill(255, 0, 0)
-    text("You've tried too many times, pick one already!", width / 2, height / 5)
+    text(inputGames[randomIndex], width / 2, height / 10);
   }
 }
 
 function buttonPressed() {
 
+  //Pushing up into array
   for (let i = 0; i < nameInputs.length; i++) {
-    games.push(nameInputs[i].value());
+    inputGames.push(nameInputs[i].value());
   }
 
   //Animate for Xms
