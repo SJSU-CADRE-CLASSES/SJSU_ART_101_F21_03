3e4d5ef34235fa534777082d26b15ed563d6370c
alejavm <89497379+alejavm@users.noreply.github.com>
2021-10-12 19:01:26 -0700

    updated 2.1

--- Assignments/AlexVilla/2.1/sketch.js
+++ Assignments/AlexVilla/2.1/sketch.js
@@ -1,18 +1,40 @@
 // dogs I have loved before
-let dogs = ["dillon", "Ziggy", "mercedes", "nala", "kloot", "petunia"];
+let dogs = [{
+  name:"dillon", 
+  color: "dog treat color"
+}, {
+  name: "Ziggy", 
+  color:"chartrese"
+}, {
+  name:"mercedes", 
+  color: "poop"
+}, {
+  name:"petunia",
+  color:"rose gold"
+}, {
+  name:"kloot",
+  color:"coffe grind brown"
+}, {
+name:"nala",
+color:"stardust"
+}];
+
+let randomIndex;
 
 function setup() {
   createCanvas(600, 600);
-
-  console.log("initial array is ");
-  console.log(dogs);
-
-  dogs.splice();
-  console.log("array after splice");
-  console.log(dogs);
+  background(200);
 
 }
 
 function draw() {
-  background(200);
+  // background(200);
+
+}
+
+function mousePressed() {
+  background(random(200, 255));
+  randomIndex = int(random(dogs.length));
+  text(dogs[randomIndex].name, 50, 50);
+  dogs.splice(randomIndex, 1);
 }
