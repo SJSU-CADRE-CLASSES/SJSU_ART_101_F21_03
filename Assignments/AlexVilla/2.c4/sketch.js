6db7eac80222e339913afd3611c2bfb39cab125d
alejavm <89497379+alejavm@users.noreply.github.com>
2021-10-18 18:42:25 -0700

    centro

--- Assignments/AlexVilla/2.c4/sketch.js
+++ Assignments/AlexVilla/2.c4/sketch.js
@@ -22,6 +22,7 @@ color:"stardust"
 let randomIndex;
 let animating = false;
 let starwars = [];
+let imageCounter = 0;
 
 function preload(){
 
@@ -35,31 +36,39 @@ function setup() {
   createCanvas(600, 600);
   background(200);
   textSize(24);
-  ImageMode(CENTER);
+  imageMode(CENTER);
+  frameRate(8);
 
-  text("click to randmonize", 50, 50);
+  text("click to randomize", 50, 50);
   console.log(starwars);
 
 }
 
 function draw() {
-
+  
   if(animating == true){
-    image(starwars[0], width/2, width/2);
+    clear();
+    image(starwars[imageCounter], width/2, height/2);
+
+    if (imageCounter < starwars.length - 1){
+    imageCounter++;
+    console.log(imageCounter);
+    } else {
+      imageCounter = 0;
+    }
   }
 }
 
 function randomizer(){
   animating = false;
-
   if (dogs[0]){
     // this displays random name and splices it out of array
-    background(random(200, 255));
+    // background(random(200, 255));
+    clear();
     randomIndex = int(random(dogs.length));
-    text(`${dogs[randomIndex].name} 's favorite color is
-    ${dogs[randomIndex].color}`, 50, 50);
-    // text(dogs[randomIndex].name + "'s favorite color is " + 
-    // dogs[randomIndex].color, 50, 50);
+    text(dogs[randomIndex].name + "'s favorite color is " + 
+    dogs[randomIndex].color, 50, 50);
+    image(random(starwars), width/2, height/2);
     dogs.splice(randomIndex, 1);
   } else {
     background(random(200, 255));
