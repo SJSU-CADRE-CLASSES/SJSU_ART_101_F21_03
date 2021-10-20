18252ab93aac74984fd45f77df8c749e5d07f543
duyenN01 <duyen.t.nguyen01@sjsu.edu>
2021-10-19 18:12:03 -0700

    changes

--- Assignments/DuyenNguyen/randomizer3/sketch.js
+++ Assignments/DuyenNguyen/randomizer3/sketch.js
@@ -98,9 +98,9 @@ function randomizer() {
     randomFlower = int(random(flowers.length));
     textSize(30);
 
-    text(flowers[randomFlower].name, width / 6, height/2);
+    text(flowers[randomFlower].name, width / 6, height/2.1);
     textSize(30);
-    text(flowers[randomFlower].color, width / 36 * 25, height/2);
+    text(flowers[randomFlower].color, width / 36 * 25, height/2.1);
     flowers.splice(randomFlower, 1);
   } else {
     background(255);
