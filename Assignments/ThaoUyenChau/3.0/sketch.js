7f867e7641e5a0623d47406c81f8986ee6c37454
ThaoU-C <89234526+ThaoU-C@users.noreply.github.com>
2021-11-03 19:21:09 -0700

    finish 3.0

--- Assignments/ThaoUyenChau/3.0/sketch.js
+++ Assignments/ThaoUyenChau/3.0/sketch.js
@@ -1,15 +1,20 @@
+let array = [];
 function setup() {
+
   createCanvas(600, 600);
-  background(150,120,220);
+  // background(150,120,220);
 
   strokeWeight(4)
+  // noFill();
 }
 
 function draw() {
   if (mouseIsPressed) {
-    stroke(map(mouseX, 0, 600, 0, 255, true));
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    // stroke(map(mouseX, 0, 600, 0, 255, true));
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    background(0);
+    array.push([mouseX, mouseY]);
   }
 
 }
@@ -21,6 +26,16 @@ function keyTyped(){
   if (key === 's'){
 
     saveCanvas('drawIng', 'png');
+   } else if (key === 'd'){
+     //  display image
+  beginShape();
+    for(let i = 0; i < array.length; i++){
+      // console.log(i);
+      // line(array[i][0], array [i][1], array[i + 1][0], array [i + 1][1]);
+      curveVertex(array[i][0], array [i][1]);
+      background(150,120,220);
+    } 
+    endShape();
     
   }
 
