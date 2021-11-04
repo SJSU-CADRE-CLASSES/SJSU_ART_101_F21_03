14ffb3b6677819701a2f0b31bdef3596e8047f9c
Nouramohd <nouraamohd@gmail.com>
2021-11-03 20:21:30 -0700

    complete

--- Assignments/NouraIdris/3.10/sketch.js
+++ Assignments/NouraIdris/3.10/sketch.js
@@ -1,19 +1,30 @@
-let array = [];
+// let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(20,52,99);
+  background(220,50,133);
 
-  strokeWeight(5);
-  noFill();
 }
 
 function draw() {
 
 if (mouseIsPressed){
-  // line(mouseX, mouseY, pmouseX, pmouseY);
-  background(0);
-  array.push([mouseX, mouseY]);
+
+  background(220,50,133, 5);
+  strokeWeight(strokeWidth);
+
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 100;
+
+
+
+  stroke(map(mouseX, 0, 600, 0, 255, true))
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  // background(0);
+  // array.push([mouseX, mouseY]);
   }
 }
 
@@ -22,17 +33,9 @@ function keyTyped(){
     if(key === 's'){
       //save this image
       saveCanvas('fileName', 'png');
-  } else if (key === 'd'){
-    //display image
-    background(255);
-
-    
-    beginShape();
-    for(let i = 0; i < array.length; i++){
-      // line(array[i] [0], array[i] [1], array[i+1] [0], array[i+1][1]);
-      curveVertex(array[i] [0], array[i] [1])
-    }
-    endShape();
+  } else if (key === 'c'){
+    //clear the image
+    clear();
 
     
    
