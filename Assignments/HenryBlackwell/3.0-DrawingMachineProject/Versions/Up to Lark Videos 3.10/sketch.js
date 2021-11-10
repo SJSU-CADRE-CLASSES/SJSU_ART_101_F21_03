5effe00df9e48e2ae5cd4f35adc0aa0c3d893cb0
Alecblackwell1 <44239082+Alecblackwell1@users.noreply.github.com>
2021-11-09 20:17:34 -0800

    trying to fix

--- Assignments/HenryBlackwell/3.0-DrawingMachineProject/Versions/Up to Lark Videos 3.10/sketch.js
+++ Assignments/HenryBlackwell/3.0-DrawingMachineProject/Versions/Up to Lark Videos 3.10/sketch.js
@@ -1,31 +1,45 @@
 let array = [];
-let backgColor = 50;
+let backgColor = (50, 123, 133, 20);
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  background(50);
+  var cnv = createCanvas(1800, 800);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / .45;
+  cnv.position(x, y);
+  background(50, 123, 133);
 
-  strokeWeight(5);
   noFill();
 }
 
 function draw() {
   
-  if (mouseIsPressed){
+  
 
+  if (mouseIsPressed){
     
     //When you're drawing, background turns black
-    backgColor -= 5;
+    // backgColor -= 3;
     background(backgColor);
+    strokeWeight(strokeWidth);
+    
+
+    noiseOffset += 0.01;
+    strokeWidth = noise(noiseOffset) * 10;
+
+
     //Push mouse position into the array
     array.push([mouseX, mouseY])
+    array.push([width - mouseY, height - mouseY])
     stroke(map(mouseX, 0, 600, 0, 255, true), map(mouseX, 0, 600, 0, 105, true), map(mouseX, 0, 600, 0, 180, true))
     line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
     line(mouseX,mouseY,pmouseX,pmouseY);
 
+    
+
   }
 
-  
 
 }
 
@@ -37,9 +51,7 @@ function keyTyped(){
     //Save the image
     saveCanvas('YourSketch', 'png')
     
-  } 
-  
-  else if (key === 'd'){
+  } else if (key === 'd'){
     background(50);
 
     //Display image with curved lines
@@ -50,9 +62,11 @@ function keyTyped(){
         curveVertex(array[i][0], array [i][1])
       }
      endShape();
-     
-
-  }
+    } else if (key === 'c') {
+      //Clear image
+      clear();
+      background(50,123,133)
+    }
   return false;
   
 }
