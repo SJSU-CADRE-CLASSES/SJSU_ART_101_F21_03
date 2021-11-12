709b17ba395252602f1c9c667e5b181f62f8e95d
alejavm <89497379+alejavm@users.noreply.github.com>
2021-11-11 18:41:07 -0800

    added 3.0

--- Assignments/AlexVilla/Project 3 - Drawing Machine/3.0/sketch.js
+++ Assignments/AlexVilla/Project 3 - Drawing Machine/3.0/sketch.js
@@ -1,12 +1,43 @@
+let array = [];
+
 function setup() {
-  createCanvas(600, 400);
-  background(220);
+  createCanvas(600, 600);
+  background(231, 145, 255);
+  textFont('Arial');
+
+  strokeWeight(5);
+  noFill();
+
 }
 
 function draw() {
 
-if (mouseIsPressed == true){
-  ellipse(mouseX, mouseY, 10, 10);
+if (mouseIsPressed){
+  // line(mouseX, mouseY, pmouseX, pmouseY);
+  background(0);
+  array.push([mouseX, mouseY]);
 }
+
+}
+
+function keyTyped(){
+
+  if (key === 's'){
+    // save this image
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd'){
+    // display image
+    background(231, 145, 255);
+  
+  beginShape();
+    for(let i = 0; i < array.length - 1; i++){
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1])
+  }
+  endShape();
+
+}
+
+  return false;
 
 }
