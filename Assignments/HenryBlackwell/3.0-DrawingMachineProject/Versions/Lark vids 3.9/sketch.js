8164387462f1b6e27282bcf0a9a5dd1c58e3115a
Alecblackwell1 <44239082+Alecblackwell1@users.noreply.github.com>
2021-11-11 19:28:52 -0800

    adding updated files

---
+++ Assignments/HenryBlackwell/3.0-DrawingMachineProject/Versions/Lark vids 3.9/sketch.js
@@ -0,0 +1,64 @@
+let array = [];
+let backgColor = 50;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(50);
+
+  strokeWeight(5);
+  noFill();
+}
+
+function draw() {
+  
+  if (mouseIsPressed){
+
+    //When you're drawing, background turns black
+    backgColor -= 5;
+    background(backgColor);
+    //Push mouse position into the array
+    array.push([mouseX, mouseY])
+    stroke(map(mouseX, 0, 600, 0, 255, true), map(mouseX, 0, 600, 0, 105, true), map(mouseX, 0, 600, 0, 180, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX,mouseY,pmouseX,pmouseY);
+
+  }
+
+  
+
+}
+
+function keyTyped(){
+  //For learning
+  console.log(`key ${key} is being typed`) 
+
+  if (key === 's'){
+    //Save the image
+    saveCanvas('YourSketch', 'png')
+    
+  } 
+  
+  else if (key === 'd'){
+    background(50);
+
+    //Display image with curved lines
+    beginShape();
+      for(let i = 0; i < array.length; i++){
+        //Array 1 and 3 are the X's and 2 and 4 are the Y's
+        // line(array[i][0], array [i][1], array[i + 1][0], array [i + 1][1]);
+        curveVertex(array[i][0], array [i][1])
+      }
+     endShape();
+     
+
+  }
+  return false;
+  
+}
+
+function mousePressed(){
+  console.log("mouse pressed function");
+  array = [];
+  console.log(array);
+  backgColor = 50;
+}
--- Assignments/HenryBlackwell/3.0-DrawingMachineProject/Versions/Up to Lark Videos 3.10/sketch.js
+++ Assignments/HenryBlackwell/3.0-DrawingMachineProject/Versions/Up to Lark Videos 3.10/sketch.js
@@ -1,45 +1,27 @@
 let array = [];
-let backgColor = (50, 123, 133, 20);
+let backgColor = 50;
 let noiseOffset = 0.0;
 let strokeWidth = 5;
 
 function setup() {
-  var cnv = createCanvas(1800, 800);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / .45;
-  cnv.position(x, y);
-  background(50, 123, 133);
+  createCanvas(windowWidth, windowHeight)
+  background(220,50,133);
 
+  strokeWeight(5);
   noFill();
 }
 
 function draw() {
-  
-  
 
-  if (mouseIsPressed){
-    
-    //When you're drawing, background turns black
-    // backgColor -= 3;
-    background(backgColor);
-    strokeWeight(strokeWidth);
-    
+  background(220,50,133, 5);
+  strokeWeight(strokeWidth);
 
-    noiseOffset += 0.01;
-    strokeWidth = noise(noiseOffset) * 10;
+  noiseOffset +- 0.05;
+  strokeWidth = noise(noiseOffset) * 100;
 
-
-    //Push mouse position into the array
-    array.push([mouseX, mouseY])
-    array.push([width - mouseY, height - mouseY])
-    stroke(map(mouseX, 0, 600, 0, 255, true), map(mouseX, 0, 600, 0, 105, true), map(mouseX, 0, 600, 0, 180, true))
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    line(mouseX,mouseY,pmouseX,pmouseY);
-
-    
-
-  }
-
+  stroke(map(mouseX, 0, 600, 0, 255, true), map(mouseX, 0, 600, 0, 105, true), map(mouseX, 0, 600, 0, 180, true))
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  line(mouseX,mouseY,pmouseX,pmouseY);
 
 }
 
@@ -51,22 +33,14 @@ function keyTyped(){
     //Save the image
     saveCanvas('YourSketch', 'png')
     
-  } else if (key === 'd'){
-    background(50);
+  } else if (key === 'c'){
+    //You would have to put a background here normally, but for this you don't need it because 
+    //the background fades in thanks to the 4th value in the background(); code
 
-    //Display image with curved lines
-    beginShape();
-      for(let i = 0; i < array.length; i++){
-        //Array 1 and 3 are the X's and 2 and 4 are the Y's
-        // line(array[i][0], array [i][1], array[i + 1][0], array [i + 1][1]);
-        curveVertex(array[i][0], array [i][1])
-      }
-     endShape();
-    } else if (key === 'c') {
-      //Clear image
-      clear();
-      background(50,123,133)
-    }
+    //Clears the image
+    clear();
+
+  }
   return false;
   
 }
