c68844aa62150563141434e3a3b91d9670b9bb52
JoodanRe <jordan.le@sjsu.edu>
2021-11-12 14:16:14 -1000

    3.3 finished
    Added draw modes: line(default), circle, square, and free(auto-cursor) draw
    
    Also added save mode to save the drawing

--- Assignments/JordanLe/3.0 ~ 3.6/sketch.js
+++ Assignments/JordanLe/3.0 ~ 3.6/sketch.js
@@ -1,3 +1,8 @@
+let circleMode = false;
+let squareMode = false;
+let lineMode = true; // default draw tool
+let freeDraw = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0, 220, 0);
@@ -8,15 +13,39 @@ function draw() {
   strokeWeight(2);
   stroke("#FF8000");
 
-  // draw circles
+  if (mouseIsPressed) {
+    if (lineMode) line(pmouseX, pmouseY, mouseX, mouseY); // same thing as line(mouseX, mouseY, pmouseX, pmouseY);
+    if (circleMode) {
+      fill(0,0,110);
+      ellipse(mouseX,mouseY,40); // draw circles
+    }
+    if (squareMode) {
+      fill(0,0,110);
+      rect(mouseX,mouseY,40); // draw rectangles
+    }
+  }
+
+  if (freeDraw) {
+    if (lineMode) line(pmouseX, pmouseY, mouseX, mouseY); // same thing as line(mouseX, mouseY, pmouseX, pmouseY);
+    if (circleMode) {
+      fill(0,0,110);
+      ellipse(mouseX,mouseY,40); // draw circles
+    }
+    if (squareMode) {
+      fill(0,0,110);
+      rect(mouseX,mouseY,40); // draw rectangles
+    }
+  }
+  /*
   if (mouseIsPressed) {
     fill(0,0,110);
-    ellipse(mouseX,mouseY,40);
+    ellipse(mouseX,mouseY,40); // draw circles
     // ellipse(width - mouseX,mouseY,40); // draw vertical symmetric circles
     // ellipse(mouseX,height - mouseY,40); // draw horizontal symmetric circles
     // ellipse(width - mouseX,height - mouseY,40); // draw inverted circles
 
-    rect(mouseX, mouseY, 40);
+    fill(0,0,110);
+    rect(mouseX, mouseY, 40); // draw squares
     // rect(width - mouseX,mouseY,40); // draw vertical symmetric rectangles
     // rect(mouseX,height - mouseY,40); // draw horizontal symmetric rectangles
     // rect(width - mouseX,height - mouseY,40); // draw inverted rectangles
@@ -36,19 +65,78 @@ function draw() {
     // rect(pmouseX, height - pmouseY, mouseX, height - mouseY); // draw horrizontal symmetric rectangles with respect to mouse position
 
     // line(mouseX, mouseY, mouseX, mouseY); // dotted lines
-    line(pmouseX, pmouseY, mouseX, mouseY); // same thing as line(mouseX, mouseY, pmouseX, pmouseY);
+    //stroke(map(mouseX, 0, width, 0, 255));
+
     // line(width - pmouseX, height - pmouseY, width - mouseX, height - mouseY); // draw inverted lines
     // line(width - pmouseX, pmouseY, width - mouseX, mouseY); // draw vertical symmetric lines
     // line(pmouseX, height - pmouseY, mouseX, height - mouseY); // draw horrizontal symmetric lines
-
 
     // prints the previous x-coordinate and the x-coordinate onto the console (ex. 772 827; where pmouseX is 772 and mouseX is 827)
     print(pmouseX + " " + mouseX);
     // line(200, 250, 100, 125); // same as line(100, 125, 200, 250);
   }
+  */
 
+  // console.log(squareMode); //only works with switch stetement?!
+  // console.log(circleMode); //works well
+}
+
+function keyTyped() {
   // erase all circles (clear all data)
-  if (keyIsPressed) {
-    background(0, 220, 0);
+  if (key === ' ')  background(0, 220, 0);
+
+  // key shortcut to activate line mode
+  if (key === 'l') {
+    // toggle line mode
+    switch (lineMode) {
+      case false:
+        lineMode = true;
+        break;
+      case true:
+        lineMode = false;
+        break;
+    }
+  }
+
+  // key shortcut to activate circle mode
+  if (key === '0')  {
+    // toggle circle mode
+    if (circleMode) {
+      circleMode = false;
+    } else {
+      circleMode = true;
+    }
+  }
+
+  // key shortcut to activate free draw
+  if (key === 'f')  {
+    // toggle circle mode
+    if (freeDraw) {
+      freeDraw = false;
+    } else {
+      freeDraw = true;
+    }
+  }
+
+  // key shortcut to activate square mode
+  if (key === '4') {
+    // toggle square mode
+
+    // why if/else doesn't work?
+    switch (squareMode) {
+      case false:
+        squareMode = true;
+        break;
+      case true:
+        squareMode = false;
+        break;
+    }
+
+    //squareMode = true;
+  }
+
+  // key shortcut to save canvas
+  if (key === 's') {
+    saveCanvas("drawMachine", "png");
   }
 }
