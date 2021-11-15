78bd1495fd72047ee873f2d7850cd2c3195f1590
alejavm <89497379+alejavm@users.noreply.github.com>
2021-11-14 17:41:47 -0800

    3.12 added

--- Assignments/AlexVilla/Project 3 - Drawing Machine/3.10/sketch.js
+++ Assignments/AlexVilla/Project 3 - Drawing Machine/3.10/sketch.js
@@ -1,21 +1,31 @@
-let array = [];
+// let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(231, 145, 255);
 
-  strokeWeight(5);
-  noFill();
+  // noFill();
 
 }
 
 function draw() {
 
-if (mouseIsPressed){
-  // line(mouseX, mouseY, pmouseX, pmouseY);
-  background(0);
-  array.push([mouseX, mouseY]);
-}
+background(231, 145, 255, 5);
+strokeWeight(strokeWidth);
+
+noiseOffset = noiseOffset += 0.05;
+strokeWidth = noise(noiseOffset) * 10;
+
+stroke(map(mouseX, 0, 600, 0, 255, true))
+line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+line(mouseX, mouseY, pmouseX, pmouseY);
+
+// if (mouseIsPressed){
+//   line(mouseX, mouseY, pmouseX, pmouseY);
+//   background(0);
+//   array.push([mouseX, mouseY]);
 
 }
 
@@ -24,16 +34,20 @@ function keyTyped(){
   if (key === 's'){
     // save this image
     saveCanvas('fileName', 'png');
-  } else if (key === 'd'){
+  } else if (key === 'e'){
+    // clear the image 
+    clear();
+
+  // else if (key === 'd'){
     // display image
-    background(231, 145, 255);
+  //   background(231, 145, 255);
   
-  beginShape();
-    for(let i = 0; i < array.length - 1; i++){
-      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array[i][1])
-  }
-  endShape();
+  // beginShape();
+  //   for(let i = 0; i < array.length - 1; i++){
+  //     // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+  //     curveVertex(array[i][0], array[i][1])
+  // }
+  // endShape();
 
 }
 
