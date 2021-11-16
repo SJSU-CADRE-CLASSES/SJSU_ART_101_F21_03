7647d98d2e306a9a9cde974b4fc5620fd05da233
ThaoU-C <89234526+ThaoU-C@users.noreply.github.com>
2021-11-16 13:35:27 -0800

    final asg

--- Assignments/ThaoUyenChau/drawingMachine/sketch.js
+++ Assignments/ThaoUyenChau/drawingMachine/sketch.js
@@ -1,24 +1,58 @@
 let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
+let xPos, yPos, dia = 80;
+let brush1 = true;
+let brush2 = true;
+let brush3 = false;
+
 function setup() {
 
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
   background(150);
   strokeWeight(4)
   drawGrid();
   noStroke();
-  rect(26,25,355,230);
+  // rect(26,25,355,230);
 
 }
 
 function draw() {
   if (mouseIsPressed) {
+  
+  xPos = mouseX
+  yPos = mouseY
+  if(brush1){
+  noStroke();
+  fill(228,239,0,80);
+  circle(xPos,yPos,dia);
+  } else {
+  brush2 = true;
+  stroke(1);
+  fill(255,179,219,60);
+  rect(xPos,yPos,dia,dia);
+  }
+
+  // if(brush3){
+  //   fill(143,200,34,120);
+  //   circle(xPos,yPos,dia); 
+  // }
+}
+
+
+
+  // if (mouseIsPressed) {
+  //   background(100, 120, 200, 10);
+  //   strokeWeight(strokeWidth);
     
-    stroke(map(mouseX, 0, 600, 0, 50, true));
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  //   noiseOffset += 0.025;
+  //   strokeWidth = noise(noiseOffset) * 100;
+  //   stroke(map(mouseX, 0, 600, 0, 50, true));
+  //   line(mouseX, mouseY, pmouseX, pmouseY);
+  //   line(width - mouseX, height - mouseY);
     // // background(0);
     // array.push([mouseX, mouseY]);
-  }
+  // }
 
 }
 
@@ -51,7 +85,7 @@ function keyTyped(){
      clear();
     drawGrid();
     noStroke();
-    rect(26,25,360,230);
+    // rect(26,25,355,230);
   // beginShape();
   //   for(let i = 0; i < array.length; i++){
   //     // console.log(i);
@@ -61,6 +95,16 @@ function keyTyped(){
   //   } 
   //   endShape();
     
+  }
+
+  if(key === '1'){
+    brush1 = !brush1;
+  }
+
+  if(key === '2'){
+    noStroke();
+    fill(random(255),random(255),random(255));
+    circle(xPos,yPos,dia); 
   }
 
   return false;
