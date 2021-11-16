25325aff67686f3e226b1c7f8fc17dbd3ad6a43c
ThaoU-C <89234526+ThaoU-C@users.noreply.github.com>
2021-11-16 15:27:05 -0800

    final asg 2

--- Assignments/ThaoUyenChau/drawingMachine/sketch.js
+++ Assignments/ThaoUyenChau/drawingMachine/sketch.js
@@ -1,7 +1,7 @@
 let array = [];
 let noiseOffset = 0.0;
-let strokeWidth = 5;
-let xPos, yPos, dia = 80;
+let strokeWidth = 1;
+let xPos, yPos, dia = 75;
 let brush1 = true;
 let brush2 = true;
 let brush3 = false;
@@ -10,38 +10,45 @@ function setup() {
 
   createCanvas(600, 600);
   background(150);
-  strokeWeight(4)
+  strokeWeight(1)
   drawGrid();
-  noStroke();
-  // rect(26,25,355,230);
 
+}
+function colorFlower(){
+  fill(random(255), random(255), random(255));
+  ellipse(xPos,yPos,20,20)
+  ellipse(xPos-15,yPos+5,20,20)
+  ellipse(xPos-25,yPos-5,20,20)
+  ellipse(xPos-17,yPos-20,20,20)
+  ellipse(xPos,yPos-15,20,20)
+  // fill(255, 230, 51);
+  fill (225, random(225), random(225));
+  ellipse(xPos-12,yPos-7,22,22) 
 }
 
 function draw() {
+
   if (mouseIsPressed) {
   
   xPos = mouseX
   yPos = mouseY
   if(brush1){
-  noStroke();
-  fill(228,239,0,80);
-  circle(xPos,yPos,dia);
+  stroke(50);
+  strokeWeight(2);
+  // noStroke();
+  fill(228,239,0,);
+  circle(xPos,yPos,dia-20);
   } else {
   brush2 = true;
-  stroke(1);
+  // fill(193,0,134);
+  stroke(193, 0, 134);
+  strokeWeight(2);
   fill(255,179,219,60);
   rect(xPos,yPos,dia,dia);
   }
-
-  // if(brush3){
-  //   fill(143,200,34,120);
-  //   circle(xPos,yPos,dia); 
-  // }
 }
 
-
-
-  // if (mouseIsPressed) {
+// if (mouseIsPressed) {
   //   background(100, 120, 200, 10);
   //   strokeWeight(strokeWidth);
     
@@ -57,6 +64,7 @@ function draw() {
 }
 
 function drawGrid(){
+
   numCells = 50;
   fillColor = 255
   strokeWeight(0);
@@ -77,6 +85,11 @@ function drawGrid(){
 function keyTyped(){
 
   console.log(`key ${key} is being typed`)
+
+  if(key === 'b'){
+    background(random(225), 225, random(225));
+
+  }
 
   if (key === 's'){
 
@@ -102,11 +115,18 @@ function keyTyped(){
   }
 
   if(key === '2'){
+    fill(random(255), random(255), random(255));
     noStroke();
-    fill(random(255),random(255),random(255));
-    circle(xPos,yPos,dia); 
+    ellipse(xPos,yPos,22,22)
+    ellipse(xPos-15,yPos+5,22,22)
+    ellipse(xPos-25,yPos-5,22,22)
+    ellipse(xPos-17,yPos-20,22,22)
+    ellipse(xPos,yPos-15,22,22)
+    // fill(255, 230, 51);
+    fill (225, random(225), random(225));
+    ellipse(xPos-12,yPos-7,25,25) 
   }
 
-  return false;
+  // return false;
 
 }
