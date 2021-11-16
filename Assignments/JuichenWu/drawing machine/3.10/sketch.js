26e9d9f4d0cb21c5cf529edd65eab3d71f54de41
daisywu1129 <91577492+daisywu1129@users.noreply.github.com>
2021-11-15 20:15:15 -0800

    upload file

--- Assignments/JuichenWu/drawing machine/3.10/sketch.js
+++ Assignments/JuichenWu/drawing machine/3.10/sketch.js
@@ -1,11 +1,10 @@
 let array =[];
-let backgroundColor =200;
 
 
 
 function setup() {
-  createCanvas(600, 600);
-   background(backgroundColor);
+  createCanvas(windowWidth, windowHeight);
+   background(220, 50, 133);
 
   strokeWeight(5);
   noFill();
@@ -17,24 +16,18 @@ function setup() {
 function draw() {
 
   if (mouseIsPressed) {
-     backgroundColor-= 3;
-// line(mouseX, mouseY,pmouseX, pmouseY);
-    background(backgroundColor);
 
-    array.push([mouseX, mouseY]);
-
-      beginShape(0);
-      for(let i =0; i < array.length; i++){
-      // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
-       curveVertex(array[i][0], array[i][1])
-     }
-     endShape();
-
-     }
-
-
-
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
+//     background(0)
+//     array.push([mouseX, mouseY]);
+
+//   }
+
+// }
 
    function keyTyped(){
 
@@ -47,7 +40,8 @@ function draw() {
      // display image
        background(255)
 
-       beginShape(0);
+       beginShape();
+
       for(let i =0; i < array.length; i++){
       // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
        curveVertex(array[i][0], array[i][1])
