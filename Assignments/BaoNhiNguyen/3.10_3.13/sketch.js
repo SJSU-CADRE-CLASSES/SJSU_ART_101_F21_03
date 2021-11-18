689b3226ae7ffc58fd4bbafa99c2b366e136787e
BanhBaoNhi <89494256+BanhBaoNhi@users.noreply.github.com>
2021-11-17 22:50:47 -0800

    change

--- Assignments/BaoNhiNguyen/3.10_3.13/sketch.js
+++ Assignments/BaoNhiNguyen/3.10_3.13/sketch.js
@@ -1,20 +1,24 @@
 let array = [];
 let backgroundColor = 200;
+let noiseOffset = 0.0;
+let strokeAmount = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // background(240, 255, 245);
   background(backgroundColor);
 
-  strokeWeight(5);
   noFill();
   //makes line of the drawing tool thicker
 
 }
 
 function draw() {
+  background(204, 204, 255, 10);
+  strokeWeight(strokeAmount);
 
-  background(204, 204, 255, 40)
+  noiseOffset += 0.05;
+  strokeAmount = noise(noiseOffset) * 100;
 
   stroke(map(mouseX, 0, 600, 0, 255, true));
   line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
@@ -27,20 +31,13 @@ function keyTyped() {
 
   if (key === 's') {
     saveCanvas('fileName', 'png');
-  } else if (key === 'd') {
 
-    //display the image
-    background(240, 255, 245);
+  } else if (key === 'c') {
+   // clears canvas   
+    clear();
 
-    beginShape();
-    for (let i = 0; i < array.length - 1; i++) {
-      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array[i][1]);
-    }
-    endShape();
-
-  }
-
+  }  
+    
 
   return false;
 }
