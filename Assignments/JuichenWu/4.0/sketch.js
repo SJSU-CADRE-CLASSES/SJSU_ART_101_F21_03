d69e30120163890d5e7429a403d90e1dd7519dc4
daisywu1129 <91577492+daisywu1129@users.noreply.github.com>
2021-11-23 18:28:19 -0800

    upload file

---
+++ Assignments/JuichenWu/4.0/sketch.js
@@ -0,0 +1,85 @@
+let array =[];
+let backgroundColor =400;
+
+
+
+function setup() {
+
+  createCanvas(900,900);
+
+background(128, 222, 234);
+  // drawGrid();
+  strokeWeight(20);
+  noFill();
+
+
+}
+
+
+function draw() {
+
+  if (mouseIsPressed) {
+
+    stroke(map(mouseX, 0, 500, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+//     background(0)
+//     array.push([mouseX, mouseY]);
+
+//   }
+
+// }
+
+   function keyTyped(){
+
+
+     if (key === 's'){
+       saveCanvas('fileName', 'png');
+     } else if (key === 'd'){
+
+
+       background(167)
+
+       beginShape();
+
+      for(let i =0; i < array.length; i++){
+      // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
+       curveVertex(array[i][0], array[i][1])
+     }
+     endShape();
+
+     }
+
+  return false;
+
+   }
+
+function mousePressed(){
+  array =[];
+  backgroundColor =280;
+}
+
+function drawGrid(){
+  numCells = 6;
+  fillColor = 265;
+   // noStroke();
+   strokeWeight(0);
+
+  for (let i = 0; i<= width; i += width / numCells){
+    for (let j = 0; j <= height; j += height / numCells){
+      if (fillColor === 255){
+      fillColor = 200;
+    } else {
+      fillColor =255;
+
+    }
+      fill(fillColor);
+      rect(i, j, width / numCells, height / numCells);
+    }
+
+     }
+
+     strokeWeight(10);
+  }
