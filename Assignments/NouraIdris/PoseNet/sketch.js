1cd164179ef3cc53ca0a68f3cac7fa1d663551cd
nouraidris <69734191+nouraidris@users.noreply.github.com>
2021-11-30 16:27:59 -0800

    human detection

--- Assignments/NouraIdris/PoseNet/sketch.js
+++ Assignments/NouraIdris/PoseNet/sketch.js
@@ -1,97 +1,63 @@
+let video;
+let poseNet;
+let pose;
 
-
-let noiseOffset = 0.0;
-let strokeWidth = 5;
-let backgroundColor= 200
-let colorPicker;
-let drawing_index = 0;
-let shapeModes = ["TRINGLES", "QUADS", "SQUARE"];
-
-function setup() {
-  createCanvas(600, 600);
-  colorPicker = createColorPicker('#ed225d');
-  colorPicker.position(600, 300);
-  // drawGrid();
-  strokeWeight();
-  noFill();
+function setup (){
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function draw() {
-  console.log("Current Index: %i", drawing_index);
-  console.log("Current Shape: %s", shapeModes[drawing_index]);
-
-if (mouseIsPressed){
-  background(backgroundColor);
-  background(colorPicker.color());
-  array.push ([mouseX,mouseY]);
-  restricted_mouse_X = mouseX;
-  restricted_mouse_Y = mouseY;
-  if (restricted_mouse_X > 255) restricted_mouse_X = 255;
-  if (restricted_mouse_Y > 255) restricted_mouse_Y = 255;
-  fill(restricted_mouse_X, restricted_mouse_Y);
-   if(shapeModes[drawing_index] == "TRIANGLES"){
-     beginShape(TRIANGLES);
-   }else if(shapeModes[drawing_index] == "QUADS"){
-     beginShape(QUADS);
-   }else if(shapeModes[drawing_index] == "SQUARE"){
-     beginShape(SQUARE);
-   }
-   else{
-     beginShape();
-   }
-   for (let i =0; i < array.length; i++){
-    curveVertex(array[i][0], array[i][1])
-      }
-    endShape();
-   }
-
-   return false;
-
-  }
-  
- 
-function keyTyped(){
- 
-    if(key === 's'){
-      //save this image
-      saveCanvas('fileName', 'png');
-  } else if (key === 'c'){
-    //clear the image
-    clear();
-  }else if(key === 'd'){
-    drawing_index++;
-    if (drawing_index > 2) {
-      drawing_index = 0;
-    }
-  }
-
-  return false;
-}
-
-function mousePressed(){
-
-  array = [];
-  backgroundColor = 255;
-}
-
-
-function drawGrid(){
-  numCells = 20;
-  fillColor = 255;
-  strokeWeight(0);
-
-  for (let i = 0; i <= width; i += width/ numCells){
-    for (let j = 0; j <=height; j += height/numCells){
-      if(fillColor == 255){
-        fillColor= 200;
-      } else {
-        fillColor = 255;
-      }
-        fill(fillColor);
-        rect(i , j,  width/ numCells ,  height/ numCells);
-
+function gotPoses(poses) {
+  console.log(poses);
+  if (poses.length > 0){
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
   }
 }
 
-strokeWeight(5);
+
+
+function modelLoaded(){
+  console.log('poseNet ready');
+}
+
+function draw(){
+  image(video, 0, 0);
+
+
+  if (pose){
+let eyeR = pose.rightEye;
+let eyeL = pose.leftEye;
+let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+
+
+  fill (255,255,255);
+  ellipse(pose.leftEye.x, pose.leftEye.y, d);
+  ellipse(pose.rightEye.x, pose.rightEye.y, d);
+
+  fill(255,50,90);
+  circle(pose.nose.x, pose.nose.y,d);
+
+  fill(255,70,90);
+ square(pose.leftShoulder.x, pose.leftShoulder.y, 70);
+  square(pose.rightShoulder.x, pose.rightShoulder.y, 70);
+
+  for (let i = 0; i < pose.keypoints.length; i ++){
+    let x = pose.keypoints[i]. position.x;
+    let y = pose.keypoints[i].position.y;
+    fill(200,255,0);
+    circle(x,y,16,16);
+  }
+for (let i = 0; i < skeleton.length; i++){
+  let a = skeleton [i][0];
+  let b = skeleton [i][1];
+  strokeWeight(2);
+  stroke(255);
+  line(a.position.x, a.position.y,b.position.x,b.position.y);
+}
+
+}
 }
